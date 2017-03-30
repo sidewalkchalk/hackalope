0bfa585ca6e9df49cd3a80eedd02e7d278de302e
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-03-30 11:35:13 -0500

    Implement webpack with live dev server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
-var path = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
 
 module.exports = {
-  entry: 'client/index.js',
+  entry: './client/index.js',
   output: {
     path: path.resolve('dist'),
     filename: 'bundle.js'
@@ -11,5 +17,6 @@ module.exports = {
       {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
       {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 }
