6365e23e8fa487fca64eb65760822172472bae26
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-04-01 14:58:26 -0500

    Add additional server middleware

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 // DEPENDENCIES
 var express = require('express');
-var app = express();
 var morgan = require ('morgan');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var passport  require('passport')
+var passport  require('passport');
 var path = require('path');
+var mongoose = require('mongoose');
 
 // ROUTES
 var auth = require('./routes/auth.js');
@@ -13,22 +13,37 @@ var main = require('./routes/main.js');
 var results = require('./routes/results.js');
 var submit = require('./routes/submit.js');
 
+// USE BLUEBIRD FOR PROMISES
+mongoose.Promise = require('bluebird');
+
+// START EXPRESS SERVER AND MONGODB
+var app = express();
+mongoose.connect('mongodb://localhost/hackalope');
+
 // MIDDLEWARE
 // TODO: implement passport
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(morgan('combined'));
 
+// SERVE STATIC FILES
+app.use('/public', express.static(path.join(__dirname, '/../client/')))
+
 // ROUTING
-app.use('/', main);
 app.use('/auth', auth);
 app.use('/results', results);
 app.use('/submit', submit);
+app.use('/', main);
 
+// CATCH 404
+app.use(function (req, res, next) {
+  var err = new Error ('Sorry--we couldn\'t find that!')
+  err.status = 404;
+  next (err);
+});
 
-// SERVER IS LISTENING
+// LISTEN
 var port = process.env.port || 8000; // also in serverConfig
 app.listen(port, function () {
   console.log('Lucifer is listening on port: ' + port + '. Build like hell!');
 });
-
