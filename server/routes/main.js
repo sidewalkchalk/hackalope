b4417219e2006e94ddf71d4d230ad7de19b600eb
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-04-01 10:43:30 -0500

    Build skeletons of routing system

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,6 +5,11 @@ var path = require('path');
 // serve static index.html page
 router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../../client/index.html'))
+});
+
+// handle search request from homepage
+router.post('/', function (req, res) {
+  // do something
 })
 
 module.exports = router;
---
+++ server/routes/results.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+// handle authentication request
+router.get('/', function (req, res) {
+  // query db for results
+})
+
+module.exports = router;
