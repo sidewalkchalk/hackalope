a9f6acee1de4b45e249313ed3e78cd1331b63bd0
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-04-01 16:47:53 -0500

    Add passport implementation (UNTESTED)

--- server/passport/login.js
+++ server/passport/login.js
@@ -4,27 +4,28 @@ var users = require('../../db/controllers/user.js');
 var login = function (passport) {
   passport.use(new LocalStrategy({
     passReqToCallback: true;
+  },
+
+  function (req, username, password, done) {
+    users.findUserByUsername(username)
+      .then(function (user) {
+        if (!user) {
+          return done(null, false);
+        }
+        return user.comparePassword (password)
+          .then(function (match) {
+            if (match) {
+              done(null, user);
+            } else {
+              done(null, false);
+            }
+          });
+      })
+      .catch(function (err) {
+        console.log('Error signing up!')
+        done(err);
+      });
   }
-    function (req, username, password, done) {
-      users.findUserByUsername(username)
-        .then(function (user) {
-          if (!user) {
-            return done(null, false);
-          }
-          return user.comparePassword (password)
-            .then(function (match) {
-              if (match) {
-                done(null, user);
-              } else {
-                done(null, false);
-              }
-            });
-        })
-        .catch(function (err) {
-          console.log('Error signing up!')
-          done(err);
-        });
-    }
   ));
 };
 
