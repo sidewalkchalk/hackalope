c8945e2d8a4f4f235ee970871eb486b4c972ca7a
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-04-01 10:12:56 -0500

    Implement up-to-date mockup

--- client/index.js
+++ client/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './components/App.jsx'
 
-ReactDOM.render(<Router />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
