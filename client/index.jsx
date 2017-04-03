5190cf160eb8591d34525819dabcd2590af6c84a
James Kip <jameskip@me.com>
2017-04-03 11:30:06 -0500

    Add - material-ui components

--- client/index.jsx
+++ client/index.jsx
@@ -6,16 +6,15 @@ import routes from './routes.jsx';
 import App from './components/app.jsx';
 import { Provider } from 'react-redux';
 
-// this is the new index that is a jsx for redux and has the <Provider> 
+// this is the new index that is a jsx for redux and has the <Provider>
 
 ReactDOM.render(
 
  <Provider store={store}>
    <App />
- </Provider>,  
+ </Provider>,
+
   document.getElementById('app')
 );
 
-//store.suscribe(render);
-
-
+store.suscribe(render);
