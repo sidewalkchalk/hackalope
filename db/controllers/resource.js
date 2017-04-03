9a8c6ffad16ad6ffbfef454bbce16d67344c8cca
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-04-03 11:27:00 -0500

    Modify for exports of functions.

--- db/controllers/resource.js
+++ db/controllers/resource.js
@@ -1,39 +1,39 @@
 var ResourceModel = require('../models/resource.js');
 
 // adds a resource to the database
-var insertResource = function (resource) {
+exports.insertResource = function (resource) {
   return ResourceModel.create(resource);
 };
 
 // finds all resources for a specified language
-var findResourcesByLanguage = function (language) {
+exports.findResourcesByLanguage = function (language) {
   return ResourceModel.findAll({language: language});
 };
 
 // finds a resource with a specified id
-var findResourceById = function (id) {
+exports.findResourceById = function (id) {
   return ResourceModel.findOne({_id: id});
 };
 
 // finds all resources with a specified tag
 // TODO: make this work for multiple tags
-var findResourcesByTag = function (language, tag) {
+exports.findResourcesByTag = function (language, tag) {
   return ResourceModel.find({language, language}, {tags, tag});
 };
 
 // finds all resources submitted by a user
-var findResourcesByUser = function (userId) {
+exports.findResourcesByUser = function (userId) {
   return ResourceModel.findAll({user: userId});
 };
 
 // deletes a resource
-var deleteResource = function (id) {
+exports.deleteResource = function (id) {
   return ResourceModel.remove({_id: id});
 };
 
 // updates a resourceSchema
 // TODO: add ability to update tags
-var updateResourceInfo = function (id, title, description, url) {
+exports.updateResourceInfo = function (id, title, description, url) {
   return ResourceModel.find({_id: id}, {$set: {
     title: title,
     description: description,
@@ -42,10 +42,10 @@ var updateResourceInfo = function (id, title, description, url) {
 };
 
 // updates a resource's rating
-var updateResourceRating = function (id, rating, modifier) {
+exports.updateResourceRating = function (id, rating, modifier) {
   var newRating = rating + modifier;
   ResourceModel.update({_id: id}, {$set: {
       rating: newRating
-    });
-  }
-}
+    };
+  });
+};
--- db/controllers/vote.js
+++ db/controllers/vote.js
@@ -1,11 +1,11 @@
 var VoteModel = require('../models/vote.js');
 
 // find the status of a user's vote on a particular resource
-var getVoteByResource = function (resourceId, userId) {
+exports.getVoteByResource = function (resourceId, userId) {
   return VoteModel.find({resource: resourceId}, {user: userId});
 };
 
-var updateVoteByResource = function (resourceId, userId, newVote) {
+exports.updateVoteByResource = function (resourceId, userId, newVote) {
   return VoteModel.update({_id: resourceId}, {user: userId}, {$set: {
     vote: newVote
   }});
