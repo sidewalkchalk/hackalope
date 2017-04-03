b08807dbca664a1e286583aa45252b1db256cbbf
Carla <carla.clay@gmail.com>
2017-04-03 17:32:48 -0500

    Redux implemented with state changes

--- client/components/resultsList.jsx
+++ client/components/resultsList.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import { Route, browserHistory, Redirect } from 'react-router';
+import { connect } from 'react-redux';
+
+// import {bindActionCreator} from 'redux';
 
 class ResultsList extends React.Component {
   
   constructor (props) {
     super (props);
   }
+  renderResults() {
+    return this.props.results.map((result) => {
+      return (
+        <li key={result.id}>
+          {result.title} {result.description}
+        </li>  
+      );
+    })
+  }
 
   render () {
-
+    return( 
+      <ul>
+        {this.renderResults()}
+      </ul>
+    );    
   }
 }
 
-export default ResultsList;
+function mapStateToProps(state) {
+  return {
+    results: state.results
+  }
+}
+
+export default connect (mapStateToProps)(ResultsList);
