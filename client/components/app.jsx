06224762cc32bb61e1ba52da8fa5670d9557250e
James Kip <jameskip@me.com>
2017-04-03 15:22:45 -0500

    Add - comments to import statements

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,16 @@
+// Required React Components
 import React from 'react';
+import { createStore } from 'redux';
+import { connect } from 'react-redux';
+import { Router, Route, hashHistory } from 'react-router';
+
+// Required Material UI Components
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Required Modules
 import Main from './main.jsx';
 import Nav from './nav.jsx';
 import routes from '../routes.jsx';
-import { createStore } from 'redux';
-import { connect } from 'react-redux';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Router, Route, hashHistory } from 'react-router';
 
 class App extends React.Component {
   constructor (props) {
