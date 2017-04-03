6602e63dd8c6541bffdb0da0a546148a8df973f7
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-04-03 11:37:09 -0500

    Add passport middleware to auth routes.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,20 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 // handle login request
-router.post('/login', function (req, res, next) {
-
-})
+router.post('/login', passport.authenticate('login'),
+  function (req, res, next) {
+    console.log(user);
+    res.sendStatus(201);
+});
 
 // handle new user signup
-router.post('/signup', function (req, res, next) {
-  passport.authenticate('signup')
+router.post('/signup', passport.authenticate('signup'),
+  function (req, res, next){
+    console.log(user);
+    res.sendStatus(201);
 });
 
 // handle logout
 router.post('/logout', function (req, res, next) {
   req.logout();
   res.sendStatus(200);
-
 });
 
 module.exports = router;
