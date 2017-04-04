2fda7021f8642874c7510f677e8c633995a779d7
James Kip <jameskip@me.com>
2017-04-04 11:56:41 -0500

    Add - react routes, *make sure you npm install

--- client/components/result.jsx
+++ client/components/result.jsx
@@ -1,30 +1,36 @@
+// Required React Components
 import React from 'react';
+
+// Required Material-UI Components
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class Result extends React.Component {
-  
+
   constructor (props) {
     super (props);
   }
 
   render () {
     return (
-    <Card>
-      <CardHeader
-        title= {this.props.result.title}
-        subtitle= {this.props.result.language}
-        actAsExpander={true}
-        showExpandableButton={true}
-      />
-      <CardActions>
-        <FlatButton label="Do some shit" />
-        <FlatButton label="Do other random shit" />
-      </CardActions>
-      <CardText expandable={true}>
-        {this.props.result.description}
-      </CardText>
-    </Card>
+    <MuiThemeProvider>
+      <Card>
+        <CardHeader
+          title= {this.props.result.title}
+          subtitle= {this.props.result.language}
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+        <CardActions>
+          <FlatButton label="Do some shit" />
+          <FlatButton label="Do other random shit" />
+        </CardActions>
+        <CardText expandable={true}>
+          {this.props.result.description}
+        </CardText>
+      </Card>
+    </MuiThemeProvider>
     );
   }
 }
