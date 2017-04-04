30b5ac00d8190f2fb7505d9e255c16bf26e59aa3
Carla <carla.clay@gmail.com>
2017-04-04 12:00:29 -0500

    Adding component testing

--- client/components/resultDetail.jsx
+++ client/components/resultDetail.jsx
@@ -1,5 +1,20 @@
+// Required React Components
 import React from 'react';
+import { createStore } from 'redux';
 import { Route, browserHistory, Redirect } from 'react-router';
+import { connect } from 'react-redux';
+import Result from './result.jsx'
+// Required Material UI Components
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// Required Modules
+import Main from './main.jsx';
+import Search from './search.jsx';
+//not using right now
+//import Comment from './comment.jsx';
+//import ResultsList from './resultsList.jsx';
+import routes from '../routes.jsx';
 
 class ResultDetail extends React.Component {
   
@@ -8,8 +23,22 @@ class ResultDetail extends React.Component {
   }
 
   render () {
-
+    return (
+      <MuiThemeProvider>
+        <div>
+          <Main />
+          <h2> We're the best around! nothing is ever gonna keep us down</h2>  
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
 
-export default ResultDetail;
+const mapStateToProps = (state) => {
+  return {
+    testreducer : state.testreducer,
+    dispatch : state.dispach
+  };
+};
+
+export default connect (mapStateToProps)(ResultDetail);
--- client/components/resultsList.jsx
+++ client/components/resultsList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, browserHistory, Redirect } from 'react-router';
 import { connect } from 'react-redux';
 import Result from './result.jsx';
-
+// V this is used for an action on redux
 // import {bindActionCreator} from 'redux';
 
 class ResultsList extends React.Component {
@@ -11,9 +11,10 @@ class ResultsList extends React.Component {
     super (props);
   }
   renderResults() {
+    //map result is each card from result.jsx
+    //card has clickable buttons already added but can use redux here for an on click action
     return this.props.results.map((result) => {
-      return (
-         
+      return ( 
         <div key = {result.id}>
           <Result key = {result.id} result = {result} />
           <br/>
@@ -37,5 +38,5 @@ function mapStateToProps(state) {
     results: state.results
   }
 }
-
+//connects redux to this component
 export default connect (mapStateToProps)(ResultsList);
