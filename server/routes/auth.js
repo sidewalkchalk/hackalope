9a7196f3d198a3927c92a26d9d69a5020f5d610c
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-04-04 10:43:58 -0500

    Bug fixes in authentication.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,14 +11,14 @@ module.exports = function (passport) {
 
   router.post('/login', passport.authenticate('login'),
   function (req, res, next) {
-    console.log(user);
+    console.log(req.body);
     res.sendStatus(201);
   });
 
   // handle new user signup
   router.post('/signup', passport.authenticate('signup'),
   function (req, res, next){
-    console.log(user);
+    console.log(req.body);
     res.sendStatus(201);
   });
 
