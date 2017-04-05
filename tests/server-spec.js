68b93382faf77cc73aba48da2e7c4e28fd720c28
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-04-04 19:29:47 -0500

    Pre rebase

--- tests/server-spec.js
+++ tests/server-spec.js
@@ -2,61 +2,29 @@ var request = require('supertest')
 var app = require('../server/server.js');
 var express = require('express');
 var chai = require('chai'), expect = chai.expect, should = chai.should();
-var mongoose = require('mongoose');
-var userController = require('../db/controllers/user.js');
-var resourceController = require('../db/controllers/resource.js');
-var userController = require('../db/controllers/user.js');
-var voteController = require('../db/controllers/vote.js');
-// set Bluebird as default promise library
-var Promise = require('bluebird');
-mongoose.Promise = Promise;
 
-//empty database before each round of testing
-// describe('', function (done) {
-//   request(app)
-//     .get('auth/logout')
-//     .end(function (err, res) {
-//         // clear all tables from the database
-//         mongoose.connect('mongodb://localhost/hackalope');
-//
-//         var users = mongoose.connection.collections.users;
-//         var resources = mongoose.connection.collections.resources;
-//         var comments = mongoose.connection.collections.comments;
-//         var votes = mongoose.connection.collections.votes;
-//         // clear all tables
-//         users.drop(() => {
-//           resources.drop(() => {
-//             comments.drop(() => {
-//               votes.drop(() => {
-//               });
-//             });
-//           });
-//         });
-//
-//       mongoose.connection.close(done());
-//
-//     })
-// });
 
-describe('Users', function (done) {
+describe('Users', () => {
 
-  it ('should be able to log in a user', function () {
+  it ('should be able to log in a user', done => {
     request(app)
-      .post('auth/login')
+      .post('/auth/login')
       .send({
         username: 'jgranny',
-        password: 'jgranny'})
+        password: 'jgranny'
+      })
       .expect(201)
+      .end(done)
+  });
+  
+  it ('should be able to access a user\'s profile page', done => {
+    request(app)
+      .get('/profile')
+      .expect(200)
       .end(done);
   });
+});
 
-  // it ('should be able to access a user\'s profile page', function () {
-  //
-  //   request(app)
-  //     .get('/profile')
-  //     .expect(200)
-  //     .end(done);
-  // });
   //
   // it ('should allow an authenticated user to submit a resource', function () {
   //
@@ -66,14 +34,13 @@ describe('Users', function (done) {
   //       title: 'Kansas',
   //       description: 'I heart Kansas',
   //       url: 'http://www.kansas.com',
-  //       tags: ['Topeka', 'Wichita'],
+  //       tags: ['Wichita', 'Topeka'],
   //       language: 'JavaScript',
   //       rating: 0,
   //     })
   //     .expect(201)
   //     .end(done);
   // })
-});
 
 // describe('Resources', function() {
 //
