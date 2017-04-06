e687b326f98a0022dbf6a7b74aa9cc9454180c88
James Kip <jameskip@me.com>
2017-04-06 15:45:26 -0500

    submit resource form resource page db responding with status 200

--- client/components/resultsList.jsx
+++ client/components/resultsList.jsx
@@ -1,18 +1,22 @@
+// Required React Components
 import React from 'react';
 import { Route, browserHistory, Redirect, Link } from 'react-router';
 import { connect } from 'react-redux';
-import Result from './result.jsx';
 import {bindActionCreators, createStore } from 'redux';
-import { selectResult } from '../actions/index.js'
+
 // V this is used for an action on redux
 // import {bindActionCreator} from 'redux';
 
-
+// Required Material UI Components
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Dialog from 'material-ui/Dialog';
 
-
+// Required Modules
+import Result from './result.jsx';
+import { selectResult } from '../actions/index.js';
+import Submit from './submit.jsx';
 
 
 
@@ -26,7 +30,6 @@ class ResultsList extends React.Component {
     //card has clickable buttons already added but can use redux here for an on click action
     return this.props.results.map((result) => {
       return (
-
         <div key = {result.id}
           onClick={() => this.props.selectResult(result)}
           style={{zDepth: 10}}
@@ -34,47 +37,41 @@ class ResultsList extends React.Component {
           <Result key = {result.id} result = {result} />
           <br/>
         </div>
-
       );
     })
   }
 
   render () {
-
     const style = {
      marginRight: 10,
       top: 'auto',
-    right: 20,
-    bottom: 20,
-    position: 'fixed',
-    
+      right: 20,
+      bottom: 20,
+      position: 'fixed',
     };
-
-    return( 
+    return(
       <MuiThemeProvider>
-
       <div style={{ display: 'inline-flex', flexDirection: 'column', width: '90%'}}>
         <div>
-
           <ul>
           {this.renderResults()}
           </ul>
-
         </div>
         <div style={{ alignSelf: 'center', width: '10%' }}>
-
-
-          <FloatingActionButton secondary={true} style={style} containerElement={<Link to='/submit' />}>
+          <FloatingActionButton
+            secondary={true} style={style}
+            containerElement={<Submit />}
+          >
             <ContentAdd />
           </FloatingActionButton>
-            
+
         </div>
       </div>
-   
+
 
       </MuiThemeProvider>
 
-    );    
+    );
   }
 }
 
