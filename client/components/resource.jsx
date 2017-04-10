2f88b58d3727159f05ca95c64bbb54b593fc56ec
Carla <carla.clay@gmail.com>
2017-04-10 15:58:16 -0500

    style resources

--- client/components/resource.jsx
+++ client/components/resource.jsx
@@ -4,8 +4,18 @@ import { createStore } from 'redux';
 import { Route, browserHistory, Redirect } from 'react-router';
 import { connect } from 'react-redux';
 
-// Required Material UI Components
+// Required Material-UI Components
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import DetailIcon from 'material-ui/svg-icons/image/details.js'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Avatar from 'material-ui/Avatar';
+import IconButton from 'material-ui/IconButton';
+import ArrowDropUp from 'material-ui/svg-icons/navigation/arrow-drop-up.js';
+import ArrowDropDown from 'material-ui/svg-icons/navigation/arrow-drop-down.js';
+import Checkbox from 'material-ui/Checkbox';
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
@@ -21,23 +31,50 @@ class Resource extends React.Component {
     super (props);
 
   }
+
+  // renderTags() {
+  //   //map result is each card from result.jsx
+  //   //card has clickable buttons, links to resource rendered on resource page
+  //   return this.props.result.tags.map( tag => {
+  //     return (
+  //       <li key = {result._id}>
+  //         <CardText key = {result.id} tag = {tag} />
+  //         <br/>
+  //       </li>
+  //     );
+  //   });
+  // };
   render () {
     if (!this.props.result) {
       return (<div>No result selected</div>);
     }
+  const style = {
+    marginRight: 10,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    position: 'fixed'
+  };
+  return (
 
-    return (
       <MuiThemeProvider>
-        <div>
-          <h2>TITLE:  {this.props.result.title}</h2>
-          <h4>LANGUAGE:  {this.props.result.language}</h4>
-          <a href = "{this.props.result.URL}">{this.props.result.URL}</a>
-          <h4>DESCRIPTION:  {this.props.result.description}</h4>
-          <h4>TAGS:  {this.props.result.tags}</h4>
-          <hr></hr>
-          <Comments />
+      <div>
+      <Card style={{ position: 'relative', fontSize: '30px', width: '100%', padding: 10 }}>
+        <CardHeader
+          title= {this.props.result.title}
+          subtitle= {this.props.result.language}
+          style={{position: 'relative', width: '60%', display: 'inline' }}
+        />
+        <div style={{ position: 'relative', display: 'inline-flex', float: 'right'}}>
         </div>
+      <CardText>
+          {this.props.result.description} <br></br>      
+        </CardText>
+      </Card> 
+      <Comments />
+      </div>
       </MuiThemeProvider>
+     
     )
   }
 }
