dfe5fd695d81f99876523c07945f3d8bdd6387ba
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-04-18 16:28:13 -0500

    Pre rebase

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-
-// var UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
 var webpack = require('webpack');
 
 module.exports = {
@@ -15,12 +13,4 @@ module.exports = {
       {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
     ]
   }
-  // plugins: [
-  //       new webpack.optimize.UglifyJsPlugin({
-  //           compress: {
-  //               warnings: false
-  //           }
-  //       }),
-  //       new UnminifiedWebpackPlugin()
-  //   ]
 }
