354b9d1bff0d4f4138e3ca2cf0c920f2ecbb3be1
James Kip <jameskip@me.com>
2017-04-18 11:24:11 -0500

    api query strings now in config object

--- server/routes/submit.js
+++ server/routes/submit.js
@@ -12,7 +12,13 @@ router.post('/', utils.checkAuth, function (req, res) {
       if (response) {
         res.send('This resource has already been posted!')
       } else {
-        axios.get('http://api.linkpreview.net/?key=58eff68ba74a41677ff8f43415db89c2157e0f9e042aa&q=' + req.body.url)
+        var config = {
+          params: {
+            key: '58eff68ba74a41677ff8f43415db89c2157e0f9e042aa',
+            q: req.body.url
+          }
+        }
+        axios.get('http://api.linkpreview.net/', config)
           .then( response => {
             var newEntry = {
               user: req.user._id,
