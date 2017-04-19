bfb8924dcae4fd3ba8f3d306aa2c45aa8624a0a2
Carla <carla.clay@gmail.com>
2017-04-19 10:45:51 -0500

    refactor usercomments

--- client/components/userComments.jsx
+++ client/components/userComments.jsx
@@ -7,38 +7,13 @@ import { userProfile } from '../actions/index.js';
 import { deleteComment } from '../helpers/commentHelpers.js';
 
 // MATERIAL UI
-<<<<<<< HEAD
 import { Card, CardHeader, CardText } from 'material-ui/Card';
 import Delete from 'material-ui/svg-icons/action/delete.js';
 
-
-
-const UserComments = ({ profile, dispatch }) => {
-  const renderUserComments = () => profile.comments.map(comment => (
-    <li key={comment._id} >
-      <Card>
-        <CardHeader
-          title={comment.body}
-          subtitle={comment.createdAt}
-        />
-        <CardText>
-              Resource : {comment.resource.title}
-          <br />
-              URL : {comment.resource.url}
-        </CardText>
-      </Card>
-    </li>
-      ));
-  return (
-    <div style={{ width: '100%', marginRight: 'auto', marginLeft: 'auto' }} >
-      <ul style={{ listStyleType: 'none' }}>
-        {renderUserComments()}
-=======
 const UserComments = ({profile, dispatch}) => {
 
-  const renderUserComments = () => {
-    return profile.comments.map(comment => {
-      return (
+  const renderUserComments = () => profile.comments.map(comment => (
+
         <li key = {comment._id} >
            <Card>
             <CardHeader
@@ -55,16 +30,12 @@ const UserComments = ({profile, dispatch}) => {
             />
           </Card>
         </li>
-      );
-    })
-  };
-  return (
-    <div style={{ width: "100%", marginRight: 'auto', marginLeft: 'auto' }} >
-      
-      <ul style={{ 'listStyleType': 'none' }}>
+      ));
 
+  return (
+    <div style={{ width: "100%", marginRight: 'auto', marginLeft: 'auto' }} >      
+      <ul style={{ 'listStyleType': 'none' }}>
       {renderUserComments()}
->>>>>>> Work on routing for delete comments button
       </ul>
     </div>
   );
