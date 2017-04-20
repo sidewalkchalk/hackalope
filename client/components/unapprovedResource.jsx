8dd3abd26049c063dff1543fef5eb1cd621c2bce
James Kip <jameskip@me.com>
2017-04-20 11:45:01 -0500

    eslint :)

--- client/components/unapprovedResource.jsx
+++ client/components/unapprovedResource.jsx
@@ -17,70 +17,70 @@ export const UnapprovedResource = ({ resource, dispatch, index }) => (
   <MuiThemeProvider>
     <li key={index} >
       <Card>
-          <CardHeader
-            title={`Submitted by ${resource.user.username} on ${resource.createdAt}`}
-            avatar={checkAvatar(resource.user)}
-          />
-          <CardTitle title={resource.title} subtitle={resource.url} />
-          <CardText>
-            <p>Edit this submission below:</p>
+        <CardHeader
+          title={`Submitted by ${resource.user.username} on ${resource.createdAt}`}
+          avatar={checkAvatar(resource.user)}
+        />
+        <CardTitle title={resource.title} subtitle={resource.url} />
+        <CardText>
+          <p>Edit this submission below:</p>
 
-            <TextField
-              name="Title"
-              value={resource.title}
-              multiLine={true}
-              style={{width: "50%"}}
-              floatingLabelText="Edit Title"
-              onChange={(e) => dispatch(editUnapproved({title: e.target.value, type: 'title', index: index}))}
-            /><br />
+          <TextField
+            name="Title"
+            value={resource.title}
+            multiLine
+            style={{ width: '50%' }}
+            floatingLabelText="Edit Title"
+            onChange={e => dispatch(editUnapproved({ title: e.target.value, type: 'title', index }))}
+          /><br />
 
-            <TextField
-              name="URL"
-              value={resource.url}
-              style={{width: "50%"}}
-              floatingLabelText="Edit URL"
-              onChange={(e) => dispatch(editUnapproved({url: e.target.value, type: 'url', index: index}))}
-            /><br />
+          <TextField
+            name="URL"
+            value={resource.url}
+            style={{ width: '50%' }}
+            floatingLabelText="Edit URL"
+            onChange={e => dispatch(editUnapproved({ url: e.target.value, type: 'url', index }))}
+          /><br />
 
-            <TextField
-              name="Description"
-              value={resource.description}
-              multiLine={true}
-              style={{width: "50%"}}
-              floatingLabelText="Edit Description"
-              onChange={(e) => dispatch(editUnapproved({description: e.target.value, type: 'description', index: index}))}
-            /><br />
+          <TextField
+            name="Description"
+            value={resource.description}
+            multiLine
+            style={{ width: '50%' }}
+            floatingLabelText="Edit Description"
+            onChange={e => dispatch(editUnapproved({ description: e.target.value, type: 'description', index }))}
+          /><br />
 
-            <TextField
-              name="Language"
-              value={resource.language}
-              multiLine={true}
-              style={{width: "50%"}}
-              floatingLabelText="Edit Language"
-              onChange={(e) => dispatch(editUnapproved({language: e.target.value, type: 'language', index: index}))}
-            /><br />
+          <TextField
+            name="Language"
+            value={resource.language}
+            multiLine
+            style={{ width: '50%' }}
+            floatingLabelText="Edit Language"
+            onChange={e => dispatch(editUnapproved({ language: e.target.value, type: 'language', index }))}
+          /><br />
 
-            <TextField
-              name="Tags"
-              value={resource.tags}
-              multiLine={true}
-              style={{width: "50%"}}
-              floatingLabelText="Add, Edit, or Remove Tags"
-              onChange={(e) => dispatch(editUnapproved({tags: e.target.value, type: 'tags', index: index}))}
-            /><br />
-          </CardText>
+          <TextField
+            name="Tags"
+            value={resource.tags}
+            multiLine
+            style={{ width: '50%' }}
+            floatingLabelText="Add, Edit, or Remove Tags"
+            onChange={e => dispatch(editUnapproved({ tags: e.target.value, type: 'tags', index }))}
+          /><br />
+        </CardText>
 
-          <CardActions>
-            <RaisedButton backgroundColor="#74FF1E" label="Approve" onClick={() => approveResource(resource, dispatch)} />
-            <RaisedButton backgroundColor="#FF3439" label="Delete" onClick={() => unapproveResource(resource._id, dispatch)} />
-          </CardActions>
-        </Card>
+        <CardActions>
+          <RaisedButton backgroundColor="#74FF1E" label="Approve" onClick={() => approveResource(resource, dispatch)} />
+          <RaisedButton backgroundColor="#FF3439" label="Delete" onClick={() => unapproveResource(resource._id, dispatch)} />
+        </CardActions>
+      </Card>
     </li>
   </MuiThemeProvider>
 );
 
-const mapStateToProps = (state) => ({
-    unapproved: state.unapproved
+const mapStateToProps = state => ({
+  unapproved: state.unapproved,
 });
 
 export default connect(mapStateToProps)(UnapprovedResource);
