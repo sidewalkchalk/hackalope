3805867bbed8b7513127420710791057f7fd6e39
James Kip <jameskip@me.com>
2017-05-06 15:36:55 -0500

    add admin styles

--- client/assets/harryStyles.js
+++ client/assets/harryStyles.js
@@ -1,7 +1,19 @@
 export const addCommentStyles = {
-    margin: 0,
-    padding: 20,
-    width: '100%',
-    textAlign: 'center',
-    display: 'inline-block',
+  margin: 0,
+  padding: 20,
+  width: '100%',
+  textAlign: 'center',
+  display: 'inline-block',
+};
+
+export const adminStyles = {
+  div: {
+    paddingLeft: 20,
+  },
+  h4: {
+    fontFamily: 'Roboto',
+  },
+  ul: {
+    listStyleType: 'none',
+  },
 };
